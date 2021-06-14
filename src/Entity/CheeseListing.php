b4b4d92f993cc51450aeb2eb55f42db9511c0baf
Norio Suzuki <norio.suzuki@gmail.com>
2021-06-15 08:06:44 +0900

    Add a CSV format

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -23,7 +23,8 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  *   },
  *   itemOperations={"get", "put"},
  *   attributes={
- *     "pagination_items_per_page"=10
+ *     "pagination_items_per_page"=10,
+ *     "formats"={"jsonld", "json", "html", "jsonhal", "csv"={"text/csv"}}
  *   },
  *   normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
  *   denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"}

2c2387018afecb4d24797f61b0906c8ff5e39beb
Norio Suzuki <norio.suzuki@gmail.com>
2021-06-18 08:08:29 +0900

    Modify to edit cheeselisting on adding new User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -59,7 +59,7 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity=CheeseListing::class, mappedBy="owner")
-     * @Groups({"user:read"})
+     * @Groups({"user:read", "user:write"})
      */
     private $cheeseListings;
 
